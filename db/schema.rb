72f50c48acddb1bbe22d23a19fd35b0647f1734a
Stanisław Chmiela <sjchmiela@gmail.com>
2013-06-04 15:04:58 +0200

    added date to event

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130604124907) do
+ActiveRecord::Schema.define(version: 20130604130423) do
 
   create_table "events", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "date"
   end
 
   create_table "events_songs", id: false, force: true do |t|
