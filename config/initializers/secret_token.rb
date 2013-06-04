e8874c4d07185efda2f23c9334c00582b5d0b3eb
Stanisław Chmiela <sjchmiela@gmail.com>
2013-06-04 14:16:21 +0200

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Songprompter::Application.config.secret_key_base = '9aaea4aeb3a422961d2ac67508cd99bdb97059f18f79129c275957d494e8fc02b0f63eb213ddeb4b41b19a908925a80731ee7078804692eb8983c66184327d69'
