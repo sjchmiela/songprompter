3de4fa23828157f170a7e78708360e02ae69f1d3
Stanisław Chmiela <sjchmiela@gmail.com>
2013-06-04 14:54:23 +0200

    migrated changes

---
+++ db/schema.rb
@@ -0,0 +1,38 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20130604124907) do
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events_songs", id: false, force: true do |t|
+    t.integer "event_id"
+    t.integer "song_id"
+    t.integer "position"
+  end
+
+  add_index "events_songs", ["event_id", "song_id"], name: "index_events_songs_on_event_id_and_song_id"
+  add_index "events_songs", ["song_id"], name: "index_events_songs_on_song_id"
+
+  create_table "songs", force: true do |t|
+    t.string   "name"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
