c36a62dd4f61af71e12b9e89486c36498ab8ffd9
Stanisław Chmiela <sjchmiela@gmail.com>
2013-06-09 15:40:11 +0200

    chorus and verses isolating

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,20 @@
 class Song < ActiveRecord::Base
   has_many :events_songs
   has_many :events, :through => :events_songs
+
+  def chorus 
+    if self.text.index("##")
+      return self.text.split("##")[0].strip.gsub(/\n/, "\n<br />")
+    else
+      return nil
+    end
+  end
+
+  def verses
+    if self.text.index("##")
+      return self.text.split("##")[1].split("\r\n\r\n").map { |x| x.strip.gsub(/\n/, "\n<br />") }
+    else
+      return self.text.split("\r\n\r\n").map { |x| x.strip.gsub(/\n/, "\n<br />") }
+    end
+  end
 end
